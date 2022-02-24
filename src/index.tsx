a49f7aabffa3fbb0eeea15aeea5b53657fd0996f
Alexsander <alextelles.dev@gmail.com>
2022-02-23 22:43:09 -0300

    feat: Criando o tema da aplicação

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from '@mui/material';
+import { LightTheme } from './shared/themes';
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
